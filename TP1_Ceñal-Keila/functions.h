6a1e5175b7d6f1ebaf9a297979d32a1713c59737
keila1088 <38932958+keila1088@users.noreply.github.com>
2021-04-08 22:18:19 -0300

    Add files via upload

---
+++ TP1_Ceñal-Keila/functions.h
@@ -0,0 +1,13 @@
+/*
+ * functions.h
+ *
+ *  Created on: 8 abr. 2021
+ *      Author: MMGyD
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+
+
+#endif /* FUNCTIONS_H_ */
---
+++ TP1_Ceñal-Keila/main_tp1.c
@@ -0,0 +1,8 @@
+/*
+ * main_tp1.c
+ *
+ *  Created on: 8 abr. 2021
+ *      Author: MMGyD
+ */
+
+
